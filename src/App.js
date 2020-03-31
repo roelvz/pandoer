fbbdf940727d8f98e5e10bc2453c90b5dbc70f58
Roel Van Zeebroeck <rvz@neton.live>
2020-03-31 20:35:33 +0200

    Trying to separate server from client

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const PandoerClient = Client({
   game: Pandoer,
   board: PandoerTable,
   numPlayers: 4,
-  multiplayer: SocketIO({server: 'localhost:8000' }),
+  multiplayer: SocketIO({server: 'https://immense-journey-70326.herokuapp.com/' }),
   debug: false,
   playerView: PlayerView.STRIP_SECRETS,
 });
