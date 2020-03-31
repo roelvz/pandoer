f462aa1b4bf7fa72d3c3e1338ad6aeacce87647f
Roel Van Zeebroeck <rvz@neton.live>
2020-03-31 20:18:16 +0200

    trying to build for heroku

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
 const Server = require('boardgame.io/server').Server;
 const Pandoer = require('./game').Pandoer;
 const server = Server({ games: [Pandoer] });
-server.run(8080);
+server.run(process.env.PORT || 8000);
